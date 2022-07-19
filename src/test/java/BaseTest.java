d6c5007457cb7221ceb6cde6b6eda8395a780850
SantiagoSola <santiagosolacosta@gmail.com>
2022-07-19 12:54:08 -0300

    updating HomePage & BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,31 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
+
+    private WebDriver driver;
+    protected HomePage homePage;
+
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.deptagency.com/");
+        homePage = new HomePage(driver);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
 }
